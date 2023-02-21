74c50bfa607fc41ab5f3a5599e4718664c56e85e
chrisdicaprio <christopher.dicaprio@gmail.com>
2023-02-22 10:16:29 +1300

    unify LOCATIONS and LOCATION_LISTS

--- tests/test_location.py
+++ tests/test_location.py
@@ -4,7 +4,15 @@ from nzshm_common import location
 def test_location_keys_unique():
     assert len(location.LOCATIONS) == len(set(loc['id'] for loc in location.LOCATIONS))
 
+def test_location_lists():
+    assert len(location.LOCATION_LISTS["NZ"]["locations"]) == 36
+    assert len(location.LOCATION_LISTS["SRWG214"]["locations"]) == 214
+    assert len(location.LOCATION_LISTS["ALL"]["locations"]) == 214 + 36
 
 def test_location_rot():
     rot = location.LOCATIONS_BY_ID['ROT']
     assert rot['name'] == 'Rotorua'
+
+def test_location_pauanui():
+    rot = location.LOCATIONS_BY_ID['srg_34']
+    assert rot['name'] == 'Pauanui'
