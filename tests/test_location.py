83a26636876b627640b74201195a83fe3df8a871
chrisdicaprio <christopher.dicaprio@gmail.com>
2023-02-22 10:49:34 +1300

    adjusted precision of srg_142 and srg_186 to match what's stored in THS db

--- tests/test_location.py
+++ tests/test_location.py
@@ -1,5 +1,5 @@
 from nzshm_common import location
-
+from nzshm_common.location.code_location import CodedLocation
 
 def test_location_keys_unique():
     assert len(location.LOCATIONS) == len(set(loc['id'] for loc in location.LOCATIONS))
@@ -16,3 +16,16 @@ def test_location_rot():
 def test_location_pauanui():
     rot = location.LOCATIONS_BY_ID['srg_34']
     assert rot['name'] == 'Pauanui'
+
+def test_rounded_locations():
+    def get_lat_lon(id):
+        return (
+            location.location_by_id(id)['latitude'],
+            location.location_by_id(id)['longitude']
+        )
+    
+    id = 'srg_142'
+    assert CodedLocation(*get_lat_lon(id), 0.001).code == "-41.520~173.948"
+
+    id = 'srg_186'
+    assert CodedLocation(*get_lat_lon(id), 0.001).code == "-44.379~171.230"
