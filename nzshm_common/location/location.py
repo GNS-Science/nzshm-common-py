206b847d1ca4d7aad9fa7cd85a237f74d38f6f76
Christopher DiCaprio <christopher.dicaprio@gmail.com>
2023-08-20 21:49:53 +1200

    compress locations json

--- nzshm_common/location/location.py
+++ nzshm_common/location/location.py
@@ -1,12 +1,14 @@
 import json
 from pathlib import Path
 
+from nzshm_common.util import decompress_string
+
 # Omitting country for now, focus on NZ
 # https://service.unece.org/trade/locode/nz.htm
 
-locations_filepath = Path(Path(__file__).parent, 'locations.json')
+locations_filepath = Path(Path(__file__).parent, 'locations.comp')
 with open(locations_filepath, 'r') as locations_file:
-    LOCATIONS = json.load(locations_file)
+    LOCATIONS = json.loads(decompress_string(locations_file.read()))
 
 nz_ids_filepath = Path(Path(__file__).parent, 'nz_ids.json')
 with open(nz_ids_filepath, 'r') as nz_ids_file:
